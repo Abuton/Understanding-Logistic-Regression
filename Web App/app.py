fb8062d1cffe8cecf221bc035db178ab4de8ee75
Abubakar <40719064+Abuton@users.noreply.github.com>
2021-02-06 23:35:57 +0100

    added bkground color

--- Web App/app.py
+++ Web App/app.py
@@ -19,9 +19,12 @@ def predict():
 	features = [int(x) for x in request.form.values()]
 	final_features = [np.array(features)]
 	prediction = model.predict(final_features)
+	if prediction == 0: prediction = 'BENIGN'
+	else: prediction = 'MALINGNANT'
 
 
-	return render_template('index.html', prediction_text=f'Result of the diagnosis test is {prediction[0]}')
+
+	return render_template('index.html', prediction_text=f'Result of the diagnosis test is {prediction}')
 
 if __name__ == '__main__':
 	app.run(debug=True, port=80)
