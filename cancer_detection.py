6976563d14eb2634f9bfadeab195a107b1caebb6
Abubakar <40719064+Abuton@users.noreply.github.com>
2021-02-18 21:33:44 +0100

    added print formatting

--- cancer_detection.py
+++ cancer_detection.py
@@ -41,5 +41,60 @@ if prediction == 0: prediction='Benign'
 else: prediction="Malignant"
 diagnosis = st.button('Get Diagnosis')
 if diagnosis:
-    st.success(prediction)
-    st.balloons()
+    st.success(f"The patient is diagnosed with {prediction} cancer")
+
+
+
+
+
+# SELECT max(speed) as maxspeed, min(speed) as minspeed,
+# avg(speed) as avgspeed, highway
+# FROM `qwiklabs-gcp-01-34dac731c423.demos.current_conditions`
+# group by highway
+
+
+# from google.cloud import storage, language_v1, bigquery
+
+# # Set up our GCS, NL, and BigQuery clients
+# storage_client = storage.Client()
+# nl_client = language_v1.LanguageServiceClient()
+# # TODO: replace YOUR_PROJECT with your project id below
+# bq_client = bigquery.Client(project='qwiklabs-gcp-04-375802f9226d')
+
+# dataset_ref = bq_client.dataset('news_classification_dataset')
+# dataset = bigquery.Dataset(dataset_ref)
+# table_ref = dataset.table('article_data') # Update this if you used a different table name
+# table = bq_client.get_table(table_ref)
+
+# # Send article text to the NL API's classifyText method
+# def classify_text(article):
+#         response = nl_client.classify_text(
+#                 document=language_v1.types.Document(
+#                         content=article,
+#                         type_='PLAIN_TEXT'
+#                 )
+#         )
+#         return response
+
+# rows_for_bq = []
+# files = storage_client.bucket('cloud-training-demos-text').list_blobs()
+# print("Got article files from GCS, sending them to the NL API (this will take ~2 minutes)...")
+
+# # Send files to the NL API and save the result to send to BigQuery
+# for file in files:
+#         if file.name.endswith('txt'):
+#                 article_text = file.download_as_string()
+#                 nl_response = classify_text(article_text)
+#                 if len(nl_response.categories) > 0:
+#                         rows_for_bq.append((str(article_text), str(nl_response.categories[0].name), str(nl_response.categories[0].confidence)))
+
+# print("Writing NL API article data to BigQuery...")
+# # Write article text + category data to BQ
+# errors = bq_client.insert_rows(table, rows_for_bq)
+# assert errors == []
+
+# import pandas as pd
+
+# percentiles = df['arrival_delay_deciles'].apply(pd.Series)
+# percentiles.rename(columns = lambda x : '{0}%'.format(x*10), inplace=True)
+# percentiles.head()
